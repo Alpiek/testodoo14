e33ff34b43d4663300f80a6f8eb8078b209eb9c0
Marc <marc@alpiek.nl>
2021-05-14 15:33:43 +0200

    added server actions

--- my_module/__manifest__.py
+++ my_module/__manifest__.py
@@ -27,6 +27,7 @@
         'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'server_actions.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
